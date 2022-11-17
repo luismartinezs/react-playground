300d8a90b15b8f420fa02b3930733216683c9733
Luis Martinez <luismartinezwebdev@gmail.com>
2022-11-17 18:52:30 +0000

    feat: radio buttons

--- src/components/RadioButtons.tsx
+++ src/components/RadioButtons.tsx
@@ -4,6 +4,7 @@ const RadioButtons = () => {
   return (
     <>
       <h2>Radio buttons</h2>
+      {/* default usage */}
       <fieldset>
         <legend>Choose your favorite color</legend>
         <input type="radio" id="red" name="color" value="red" />
@@ -12,6 +13,27 @@ const RadioButtons = () => {
         <label htmlFor="green">Green</label>
         <input type="radio" id="blue" name="color" value="blue" />
         <label htmlFor="blue">Blue</label>
+      </fieldset>
+      {/* doesn't work */}
+      <span id="legend">Choose your favorite animal</span>
+      <fieldset>
+        <legend aria-describedby="legend" aria-label=""></legend>
+        <input type="radio" id="dog" name="animal" value="dog" />
+        <label htmlFor="dog">Dog</label>
+        <input type="radio" id="cat" name="animal" value="cat" />
+        <label htmlFor="cat">Cat</label>
+        <input type="radio" id="bird" name="animal" value="bird" />
+        <label htmlFor="bird">Bird</label>
+      </fieldset>
+      {/* Works! */}
+      <fieldset>
+        <legend aria-label="Choose your favorite food"></legend>
+        <input type="radio" id="pizza" name="food" value="pizza" />
+        <label htmlFor="pizza">Pizza</label>
+        <input type="radio" id="burger" name="food" value="burger" />
+        <label htmlFor="burger">Burger</label>
+        <input type="radio" id="salad" name="food" value="salad" />
+        <label htmlFor="salad">Salad</label>
       </fieldset>
     </>
   );
