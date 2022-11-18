728761b7e80848b9fcba56ea2f63cc3f5715b5ed
Luis Martinez <luismartinezwebdev@gmail.com>
2022-11-18 18:57:26 +0000

    feat: style fieldset experiment

--- src/components/FieldsetExperiments/FieldsetExperiments.tsx
+++ src/components/FieldsetExperiments/FieldsetExperiments.tsx
@@ -1,5 +1,26 @@
 import React from "react";
 
+type Props = {
+  id: string;
+  label: string;
+  value: string;
+};
+
+const RadioRole = ({ id, label, value }: Props) => {
+  return (
+    <div className="flex items-center justify-center mb-1 border w-14 border-sky-500 focus-within:bg-sky-500 focus-within:border-sky-700 focus-within:text-white">
+      <span
+        role="radio"
+        aria-checked="false"
+        tabIndex={0}
+        aria-labelledby={id}
+        data-value={value}
+      ></span>
+      <label id={id}>{label}</label>
+    </div>
+  );
+};
+
 const FieldsetExperiments = () => {
   return (
     <>
@@ -10,36 +31,9 @@ const FieldsetExperiments = () => {
       </p>
       <fieldset>
         <legend>Ipsum and lorem?</legend>
-        <div className="focus-within:font-bold">
-          <span
-            role="radio"
-            aria-checked="false"
-            tabIndex={0}
-            aria-labelledby="q25_radio1-label"
-            data-value="True"
-          ></span>
-          <label id="q25_radio1-label">True</label>
-        </div>
-        <div className="focus-within:font-bold">
-          <span
-            role="radio"
-            aria-checked="false"
-            tabIndex={0}
-            aria-labelledby="q25_radio2-label"
-            data-value="False"
-          ></span>
-          <label id="q25_radio2-label">False</label>
-        </div>
-        <div className="focus-within:font-bold">
-          <span
-            role="radio"
-            aria-checked="true"
-            tabIndex={0}
-            aria-labelledby="q25_radio3-label"
-            data-value="huh?"
-          ></span>
-          <label id="q25_radio3-label">What is the question?</label>
-        </div>
+        <RadioRole id="q25_radio1" label="True" value="True" />
+        <RadioRole id="q25_radio2" label="False" value="False" />
+        <RadioRole id="q25_radio3" label="Maybe" value="Maybe" />
       </fieldset>
     </>
   );
