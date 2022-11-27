8128574cd3a47aedd20e1332ea30f81782392b59
Luis Martinez <luismartinezwebdev@gmail.com>
2022-11-27 23:25:07 +0000

    feat: normal date input

--- src/components/DateInput.tsx
+++ src/components/DateInput.tsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const DateInput = () => {
+  const input = React.useRef<HTMLInputElement>(null);
+  const [showInput, setShowInput] = React.useState(true);
+
+  const maskedProps = {
+    type: "text",
+    value: "**/**/****",
+    // onFocus: () => setShowInput(true),
+  };
+
+  const unmaskedProps = {
+    type: "date",
+    // onBlur: (e: React.FocusEvent<HTMLInputElement>) => {
+    //   setShowInput(false);
+    // },
+  };
+
   return (
     <div>
-      <h2>DateInput</h2>
+      <h2>Toggle Date Input</h2>
 
       <label htmlFor="dob" className="block">
         Date of birth
       </label>
-      <input type="date" id="dob" name="dob" autoComplete="bday" />
+      <input
+        ref={input}
+        id="dob"
+        name="dob"
+        autoComplete="bday"
+        {...(showInput ? unmaskedProps : maskedProps)}
+      />
     </div>
   );
 };
