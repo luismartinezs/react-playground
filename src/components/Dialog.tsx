8e41629b70545d7a8b69530228560a8a06ac4a14
Luis Martinez <luismartinezwebdev@gmail.com>
2022-11-28 12:53:12 +0000

    feat: able to set toast duration in dialog

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -3,6 +3,7 @@ import { SROnlyToast } from "./ScreenReader";
 
 const Dialog = () => {
   const [isOpen, setIsOpen] = React.useState(false);
+  const [timeout, setTimeout] = React.useState(5000);
 
   return (
     <div>
@@ -10,13 +11,24 @@ const Dialog = () => {
       <p>Dialog title gets announced when focus is placed inside the dialog</p>
       <p>the dialog contains a live assertive toast</p>
 
+      <div>
+        <label>
+          Timeout for alert:&nbsp;
+          <input
+            type="number"
+            value={timeout}
+            onChange={(e) => setTimeout(+e.target.value)}
+          />
+        </label>
+      </div>
+
       {isOpen ? (
         <div
           role="dialog"
           aria-labelledby="dialog-title"
           className="p-2 my-2 border border-sky-500"
         >
-          <SROnlyToast timeout={5000}>
+          <SROnlyToast timeout={timeout}>
             <div aria-live="assertive">
               Chuck Norris is the most powerful man in the universe
             </div>
