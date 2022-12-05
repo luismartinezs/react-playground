6c9f1c14e4bf8a62d90a687aab694824553e8a1c
Luis Martinez <luismartinezwebdev@gmail.com>
2022-12-05 21:40:02 +0000

    feat: blurred input works with firefox

--- src/components/BlurredInput/BlurredInput.tsx
+++ src/components/BlurredInput/BlurredInput.tsx
@@ -5,16 +5,23 @@ import classnames from "classnames";
 import Styles from "./BlurredInput.module.scss";
 
 const BlurredInput = () => {
+  const mask = "**/**/****";
+
   return (
     <div>
       <h2>BlurredInput</h2>
       <label htmlFor="blurred-input">
         Date of birth
-        <input
-          className={classnames(Styles.blurredInput, "block")}
-          type="date"
-          id="blurred-input"
-        />
+        <div className={Styles.inputWrapper}>
+          <input
+            className={classnames(Styles.input, "block")}
+            type="date"
+            id="blurred-input"
+          />
+          <span aria-hidden="true" className={classnames(Styles.inputMask)}>
+            {mask}
+          </span>
+        </div>
       </label>
     </div>
   );
