10603ec186b41e5d9e3eaa64e09331aeaba00af0
Luis Martinez <luismartinezwebdev@gmail.com>
2023-01-11 18:29:52 +0000

    feat: elrond

--- src/components/StatusOnLoad/StatusOnLoad.tsx
+++ src/components/StatusOnLoad/StatusOnLoad.tsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react'
 
-const MSG = { bob: 'Status Bob', charles: 'Status Charles', diane: 'Status Diane' }
+const MsgBox = ({ children }: { children?: React.ReactNode }) => {
+  return <div className="p-2 m-1 border rounded-md border-sky-500 w-fit min-w-[150px] min-h-[42px]">{children}</div>
+}
+
+const MSG = { bob: 'Status Bob', charles: 'Status Charles', diane: 'Status Diane', elrond: 'Status Elrond' }
 
 const StatusOnLoad = () => {
   const [bob, setBob] = useState(MSG.bob)
   const [charles, setCharles] = useState(MSG.charles)
   const [diane, setDiane] = useState(MSG.diane)
+  const [elrond, setElrond] = useState('')
 
   useEffect(() => {
     setBob('')
@@ -28,26 +33,46 @@ const StatusOnLoad = () => {
     return () => clearTimeout(timer)
   })
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setElrond(MSG.elrond)
+    }, 5000)
+    return () => clearTimeout(timer)
+  })
+
   return (
     <div>
       <h2>Status</h2>
       <p>
         Alice message is rendered as is. Bob message is rendered, then set to empty, then rendered, immediately on
-        mount. Charles message is like Bob, but with 500ms delay. Diane, after 5000ms
+        mount. Charles message is like Bob, but with 500ms delay. Diane, after 5000ms. Elrond pops up after 5000ms.
       </p>
       <div>
-        <div role="status" aria-live="polite">
-          Status Alice
-        </div>
-        <div role="status" aria-live="polite">
-          {bob}
-        </div>
-        <div role="status" aria-live="polite">
-          {charles}
-        </div>
-        <div role="status" aria-live="polite">
-          {diane}
-        </div>
+        <MsgBox>
+          <div role="status" aria-live="polite">
+            Status Alice
+          </div>
+        </MsgBox>
+        <MsgBox>
+          <div role="status" aria-live="polite">
+            {bob}
+          </div>
+        </MsgBox>
+        <MsgBox>
+          <div role="status" aria-live="polite">
+            {charles}
+          </div>
+        </MsgBox>
+        <MsgBox>
+          <div role="status" aria-live="polite">
+            {diane}
+          </div>
+        </MsgBox>
+        <MsgBox>
+          <div role="status" aria-live="polite">
+            {elrond}
+          </div>
+        </MsgBox>
       </div>
     </div>
   )
