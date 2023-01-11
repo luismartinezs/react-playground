28f99c9eb0f5fda7c92b481b3eb53c1d656a54a0
Luis Martinez <luismartinezwebdev@gmail.com>
2023-01-11 18:19:46 +0000

    feat: diane

--- src/components/StatusOnLoad/StatusOnLoad.tsx
+++ src/components/StatusOnLoad/StatusOnLoad.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 
-const MSG = { bob: 'Status Bob', charles: 'Status Charles' }
+const MSG = { bob: 'Status Bob', charles: 'Status Charles', diane: 'Status Diane' }
 
 const StatusOnLoad = () => {
   const [bob, setBob] = useState(MSG.bob)
   const [charles, setCharles] = useState(MSG.charles)
+  const [diane, setDiane] = useState(MSG.diane)
 
   useEffect(() => {
     setBob('')
@@ -19,12 +20,20 @@ const StatusOnLoad = () => {
     return () => clearTimeout(timer)
   })
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDiane('')
+      setDiane(MSG.diane)
+    }, 5000)
+    return () => clearTimeout(timer)
+  })
+
   return (
     <div>
       <h2>Status</h2>
       <p>
         Alice message is rendered as is. Bob message is rendered, then set to empty, then rendered, immediately on
-        mount. Charles message is like Bob, but with 500ms delay
+        mount. Charles message is like Bob, but with 500ms delay. Diane, after 5000ms
       </p>
       <div>
         <div role="status" aria-live="polite">
@@ -35,6 +44,9 @@ const StatusOnLoad = () => {
         </div>
         <div role="status" aria-live="polite">
           {charles}
+        </div>
+        <div role="status" aria-live="polite">
+          {diane}
         </div>
       </div>
     </div>
