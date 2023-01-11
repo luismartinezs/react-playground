09c3a6ee9148f0d47dba8b215fef5bd30e5182a7
Luis Martinez <luismartinezwebdev@gmail.com>
2023-01-11 17:10:26 +0000

    feat: charles

--- src/components/StatusOnLoad/StatusOnLoad.tsx
+++ src/components/StatusOnLoad/StatusOnLoad.tsx
@@ -1,24 +1,40 @@
 import React, { useEffect, useState } from 'react'
 
-const MSG = 'Status Bob'
+const MSG = { bob: 'Status Bob', charles: 'Status Charles' }
 
 const StatusOnLoad = () => {
-  const [bob, setBob] = useState(MSG)
+  const [bob, setBob] = useState(MSG.bob)
+  const [charles, setCharles] = useState(MSG.charles)
 
   useEffect(() => {
     setBob('')
-    setBob(MSG)
+    setBob(MSG.bob)
+  }, [])
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCharles('')
+      setCharles(MSG.charles)
+    }, 500)
+    return () => clearTimeout(timer)
   })
 
   return (
     <div>
       <h2>Status</h2>
+      <p>
+        Alice message is rendered as is. Bob message is rendered, then set to empty, then rendered, immediately on
+        mount. Charles message is like Bob, but with 500ms delay
+      </p>
       <div>
         <div role="status" aria-live="polite">
           Status Alice
         </div>
         <div role="status" aria-live="polite">
           {bob}
+        </div>
+        <div role="status" aria-live="polite">
+          {charles}
         </div>
       </div>
     </div>
