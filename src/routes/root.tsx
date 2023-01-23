6c39d82a2e60dd8e4d450598703311262ed09f2c
Luis Martinez <luismartinezwebdev@gmail.com>
2023-01-23 13:25:45 +0000

    style: clearer nav

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -47,11 +47,12 @@ export default function Root() {
       <h1 className="text-lg font-bold">React playground</h1>
       <nav className="pb-2 mb-2 border-b border-sky-500">
         <ul className="flex flex-wrap w-full space-x-2">
-          {routes.map((route) => (
+          {routes.map((route, idx) => (
             <li key={route.path}>
               <a href={route.path} className="whitespace-nowrap">
                 {route.label}
               </a>
+              {idx < routes.length - 1 && <span className="inline-block ml-2">-</span>}
             </li>
           ))}
         </ul>
