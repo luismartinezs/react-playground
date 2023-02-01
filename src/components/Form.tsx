70698fd1fd69397b2a8edfbae7519065f8be2ea1
Luis Martinez <luismartinezwebdev@gmail.com>
2023-02-01 19:09:55 +0000

    feat: hey check this box

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,18 +1,19 @@
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler } from 'react-hook-form'
 
 interface IFormInputs {
-  name: string;
-  mail: string;
+  name: string
+  mail: string
+  checkbox: boolean
 }
 
-const onSubmit: SubmitHandler<IFormInputs> = (data) => console.log(data);
+const onSubmit: SubmitHandler<IFormInputs> = (data) => console.log(data)
 
-export default function Form() {
+export default function Form({ disabledCheckbox = true }: { disabledCheckbox: boolean }) {
   const {
     register,
     formState: { errors },
     handleSubmit,
-  } = useForm<IFormInputs>();
+  } = useForm<IFormInputs>()
 
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
@@ -23,14 +24,12 @@ export default function Form() {
           Name
           <input
             id="name"
-            {...register("name", { required: true })}
-            aria-invalid={errors.name ? "true" : "false"}
-            aria-describedby={errors.name ? "errors-name" : undefined}
+            {...register('name', { required: true })}
+            aria-invalid={errors.name ? 'true' : 'false'}
+            aria-describedby={errors.name ? 'errors-name' : undefined}
           />
         </label>
-        {errors.name?.type === "required" && (
-          <p id="errors-name">First name is required</p>
-        )}
+        {errors.name?.type === 'required' && <p id="errors-name">First name is required</p>}
       </div>
 
       {/* include validation with required or other standard HTML validation rules */}
@@ -39,18 +38,28 @@ export default function Form() {
           Mail
           <input
             id="mail"
-            {...register("mail", { required: "Email Address is required" })}
-            aria-invalid={errors.mail ? "true" : "false"}
-            aria-describedby={errors.mail ? "errors-email" : undefined}
+            {...register('mail', { required: 'Email Address is required' })}
+            aria-invalid={errors.mail ? 'true' : 'false'}
+            aria-describedby={errors.mail ? 'errors-email' : undefined}
             className="mt-2"
           />
         </label>
         {errors.mail && <p id="errors-email">{errors.mail?.message}</p>}
       </div>
 
+      {/* Disabled but focusable checkbox */}
       <div>
-        <input type="submit" className="button mt-2" />
+        <input
+          type="checkbox"
+          {...(disabledCheckbox ? { ...register('checkbox'), checked: false } : register('checkbox'))}
+          aria-disabled={disabledCheckbox}
+        />
+        <label htmlFor="checkbox">Checkbox</label>
+      </div>
+
+      <div>
+        <input type="submit" className="mt-2 button" />
       </div>
     </form>
-  );
+  )
 }
